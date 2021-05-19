94c6ceb3d17be923fde9455d2018692b8399ebdf
Coding-Tams <82816924+Coding-Tams@users.noreply.github.com>
2021-05-19 10:43:29 +0100

    Create reading list.js
    Add psudocode for reading list problem then solution

---
+++ reading list.js
@@ -0,0 +1,59 @@
+/*
+
+Reading List 
+
+1. create a list of books
+    array of objects
+    each OBJECT is a BOOK
+    each BOOK has a TITLE and AUTHOR
+    if i have not read = BOOLEAN / true or false
+    each book had a TITLE and a AUTHOR 
+if I have not read it or not BOOLEAN / true or false
+
+Books = [
+    Title = string
+    Author = string
+    hasRead = BOOLEAN
+    
+]
+
+const books = [
+    {
+        title: ""
+        author: ""
+        hasRead: false
+    }
+]
+2. For Loop
+    counter = 0
+    counter < books, length
+    counter +1
+
+    4. show wheather you have read the book
+If read = TRUE
+    THEN I have read the book
+    Else I have not read
+
+    If hasRead
+    Then 
+
+    */
+
+    const books = [
+        {
+        title: 'Moomins';
+        author: 'Tove Jansson';
+        hasRead: true;
+        },
+        {
+        title: 'Lord of the Rings';
+        author: 'JRR Tolken'
+        hasRead: false;
+        }
+    ]
+
+for(let counter = 0; counter < book.length; counter++) {
+
+}
+if(books[counter].hasRead)
+
